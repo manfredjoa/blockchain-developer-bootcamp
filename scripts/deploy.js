303ec7eb6dc10e79ac07cd50157dadb9179c5367
manfredjoa <joamanfred@gmail.com>
2023-12-19 18:30:24 -0500

    update deploy script to deploy tokens and exchange

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,16 +1,38 @@
-const main = async () => {
+const deploy = async () => {
+  console.log("Preparing deployment...\n");
+
   // Fetch contract to deploy
   const Token = await ethers.getContractFactory("Token");
+  const Exchange = await ethers.getContractFactory("Exchange");
 
-  // Deploy contract
-  const fetchedToken = await Token.deploy();
+  // Fetch accounts
+  const accounts = await ethers.getSigners();
 
-  await fetchedToken.deployed();
+  console.log(
+    `Accounts fetched:\n${accounts[0].address}\n${accounts[1].address}\n`
+  );
 
-  console.log(`Token deployed to: ${fetchedToken.address}`);
+  // Deploy contracts
+  const dApp = await Token.deploy("dApp University", "DAPP", "1000000");
+  await dApp.deployed();
+  console.log(`DAPP deployed to: ${dApp.address}`);
+
+  const mETH = await Token.deploy("mETH", "mETH", "1000000");
+  await mETH.deployed();
+  console.log(`mETH deployed to: ${mETH.address}`);
+
+  const mDAI = await Token.deploy("mDAI", "mDAI", "1000000");
+  await mDAI.deployed();
+  console.log(`mDAI deployed to: ${mDAI.address}`);
+
+  const exchange = await Exchange.deploy(accounts[1].address, 10);
+  await exchange.deployed();
+  console.log(`Exchange deployed to ${exchange.address}`);
 };
 
-main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
+deploy()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
