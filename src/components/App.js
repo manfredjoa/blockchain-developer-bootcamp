077c7e031e0313d7273bbbc2fc5e050091e015b4
manfredjoa <joamanfred@gmail.com>
2023-12-21 15:29:14 -0500

    refactor to use interactions from redux store

--- src/components/App.js
+++ src/components/App.js
@@ -1,34 +1,25 @@
 import { useEffect } from "react";
-import { ethers } from "ethers";
-import TOKEN_ABI from "../abis/Token.json";
+import { useDispatch } from "react-redux";
 import config from "../config.json";
-import "../App.css";
+import {
+  loadProvider,
+  loadNetwork,
+  loadAccount,
+  loadToken,
+} from "../store/interactions";
 
 const App = () => {
-  const loadBlockchainData = async () => {
-    const accounts = await window.ethereum.request({
-      method: "eth_requestAccounts",
-    });
+  const dispatch = useDispatch();
 
-    console.log(accounts); // Somehow my accounts got swapped when giving my localhost permission to my MetaMask accounts, so Hardhat #1 is accounts[0] and Hardhat #0 is accounts[1]
+  const loadBlockchainData = async () => {
+    await loadAccount(dispatch);
 
     // Connect Ethers to blockchain
-    const provider = new ethers.providers.Web3Provider(window.ethereum);
-    const { chainId } = await provider.getNetwork();
-
-    console.log(chainId);
-    console.log(config);
+    const provider = loadProvider(dispatch);
+    const chainId = await loadNetwork(provider, dispatch);
 
     // Token Smart Contract
-    const token = new ethers.Contract(
-      config[chainId].dApp.address,
-      TOKEN_ABI,
-      provider
-    );
-    const symbol = await token.symbol();
-
-    console.log(token.address);
-    console.log(symbol);
+    await loadToken(provider, config[chainId].dApp.address, dispatch);
   };
 
   useEffect(() => {
