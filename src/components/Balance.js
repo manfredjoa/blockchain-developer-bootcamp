14748972b18b175be1b165eda08d6c8a4464ad16
manfredjoa <joamanfred@gmail.com>
2023-12-27 16:46:33 -0500

    add withdrawHandler and ternary for form onSubmit

--- src/components/Balance.js
+++ src/components/Balance.js
@@ -71,6 +71,34 @@ const Balance = () => {
     }
   };
 
+  const withdrawHandler = (e, token) => {
+    e.preventDefault();
+
+    if (token.address === tokens[0].address) {
+      transferTokens(
+        provider,
+        exchange,
+        "Withdraw",
+        token,
+        token1TransferAmount,
+        dispatch
+      );
+
+      setToken1TransferAmount(0);
+    } else {
+      transferTokens(
+        provider,
+        exchange,
+        "Withdraw",
+        token,
+        token2TransferAmount,
+        dispatch
+      );
+
+      setToken2TransferAmount(0);
+    }
+  };
+
   useEffect(() => {
     if (exchange && tokens[0] && tokens[1] && account)
       loadBalances(exchange, tokens, account, dispatch);
@@ -118,7 +146,13 @@ const Balance = () => {
           </p>
         </div>
 
-        <form onSubmit={(e) => depositHandler(e, tokens[0])}>
+        <form
+          onSubmit={
+            isDeposit
+              ? (e) => depositHandler(e, tokens[0])
+              : (e) => withdrawHandler(e, tokens[0])
+          }
+        >
           <label htmlFor="token0">{symbols && symbols[0]} Amount</label>
           <input
             type="text"
@@ -162,7 +196,13 @@ const Balance = () => {
           </p>
         </div>
 
-        <form onSubmit={(e) => depositHandler(e, tokens[1])}>
+        <form
+          onSubmit={
+            isDeposit
+              ? (e) => depositHandler(e, tokens[1])
+              : (e) => withdrawHandler(e, tokens[1])
+          }
+        >
           <label htmlFor="token1">{symbols && symbols[1]} Amount</label>
           <input
             type="text"
