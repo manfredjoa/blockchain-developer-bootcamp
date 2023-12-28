41149981eecf31056f9cffc1fbfcb3cf34acf152
manfredjoa <joamanfred@gmail.com>
2023-12-28 00:12:19 -0500

    add orders loaded cases for cancelled, filled, and all orders

--- src/store/reducers.js
+++ src/store/reducers.js
@@ -94,6 +94,37 @@ export const exchange = (state = DEFAULT_EXCHANGE_STATE, action) => {
         contract: action.exchange,
       };
 
+    // Orders Loaded (Cancelled, Filled, & All)
+
+    case "CANCELLED_ORDERS_LOADED":
+      return {
+        ...state,
+        cancelledOrders: {
+          loaded: true,
+          data: action.cancelledOrders,
+        },
+      };
+
+    case "FILLED_ORDERS_LOADED":
+      return {
+        ...state,
+        filledOrders: {
+          loaded: true,
+          data: action.filledOrders,
+        },
+      };
+
+    case "ALL_ORDERS_LOADED":
+      return {
+        ...state,
+        allOrders: {
+          loaded: true,
+          data: action.allOrders,
+        },
+      };
+
+    // Balance Cases
+
     case "EXCHANGE_TOKEN_1_BALANCE_LOADED":
       return {
         ...state,
@@ -158,7 +189,7 @@ export const exchange = (state = DEFAULT_EXCHANGE_STATE, action) => {
     case "NEW_ORDER_SUCCESS":
       // Prevent duplicate orders
       index = state.allOrders.data.findIndex(
-        (order) => order.id === action.orderId
+        (order) => order.id.toString() === action.order.id.toString()
       );
 
       if (index === -1) data = [...state.allOrders.data, action.order];
