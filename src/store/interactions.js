dc990dab333baaaffea40f85bd0ea56f102cfdf3
manfredjoa <joamanfred@gmail.com>
2024-01-01 18:26:33 -0500

    add cancelOrder function

--- src/store/interactions.js
+++ src/store/interactions.js
@@ -57,6 +57,24 @@ export const loadExchange = async (provider, address, dispatch) => {
 };
 
 export const subscribeToEvents = (exchange, dispatch) => {
+  exchange.on(
+    "Cancel",
+    (
+      id,
+      user,
+      tokenGet,
+      amountGet,
+      tokenGive,
+      amountGive,
+      timestamp,
+      event
+    ) => {
+      const order = event.args;
+
+      dispatch({ type: "ORDER_CANCEL_SUCCESS", order, event });
+    }
+  );
+
   exchange.on("Deposit", (token, user, amount, balance, event) => {
     dispatch({ type: "TRANSFER_SUCCESS", event });
   });
@@ -226,5 +244,19 @@ export const makeSellOrder = async (
     await transaction.wait();
   } catch (error) {
     dispatch({ type: "NEW_ORDER_FAIL" });
+  }
+};
+
+// Cancel Orders
+
+export const cancelOrder = async (provider, exchange, order, dispatch) => {
+  dispatch({ type: "ORDER_CANCEL_REQUEST" });
+
+  try {
+    const signer = await provider.getSigner();
+    const transaction = await exchange.connect(signer).cancelOrder(order.id);
+    await transaction.wait();
+  } catch (error) {
+    dispatch({ type: "ORDER_CANCEL_FAIL" });
   }
 };
