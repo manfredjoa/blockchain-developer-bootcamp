455129dfb23ce975471ba497f0bf3a87cf57696f
manfredjoa <joamanfred@gmail.com>
2024-01-02 18:56:33 -0500

    add mumbai to networks

--- hardhat.config.js
+++ hardhat.config.js
@@ -11,5 +11,9 @@ module.exports = {
       url: `https://sepolia.infura.io/v3/${process.env.INFURA_API_KEY}`,
       accounts: privateKeys.split(", "),
     },
+    mumbai: {
+      url: `https://polygon-mumbai.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`,
+      accounts: privateKeys.split(", "),
+    },
   },
 };
